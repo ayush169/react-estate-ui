0b5d1e8cf70564c099fd9a133e2a57f25e0a6ce1
Ayush Dubey <ayushdubey@Ayushs-MacBook-Pro.local>
2024-06-20 20:28:35 +0530

    hamburger menu design

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "./navbar.scss";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav>
       <div className="left">
@@ -18,6 +21,21 @@ function Navbar() {
         <a href="/" className="register">
           Sign up
         </a>
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
       </div>
     </nav>
   );
